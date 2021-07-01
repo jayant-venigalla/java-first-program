4c77b742713cf229096a94d433c9a6257eec0009
jayant-venigalla <86660764+jayant-venigalla@users.noreply.github.com>
2021-07-01 18:00:14 +0530

    Module 2 code updates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -9,9 +9,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int[] x = {1,2,3};
+        System.out.println(add(x));
+
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number*2;
     }
+
+    private static int add(int[] numbers){
+        int sum = 0;
+        for (int i:numbers) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
 }
